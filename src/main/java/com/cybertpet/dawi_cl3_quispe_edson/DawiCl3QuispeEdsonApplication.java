d3dce8c210d5ce82139610bc3124609634302aff
unknown <SUITE@SUITEBG210-ST12.cibertec.edu.pe>
2023-06-19 15:23:57 -0500

    My first commit

---
+++ src/main/java/com/cybertpet/dawi_cl3_quispe_edson/DawiCl3QuispeEdsonApplication.java
@@ -0,0 +1,13 @@
+package com.cybertpet.dawi_cl3_quispe_edson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3QuispeEdsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3QuispeEdsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybertpet/dawi_cl3_quispe_edson/DawiCl3QuispeEdsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybertpet.dawi_cl3_quispe_edson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3QuispeEdsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
